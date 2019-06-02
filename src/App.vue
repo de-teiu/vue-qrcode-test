8734a91b02307d3d8c0a9f291f7cbb977db8d78a
LAPTOP-SGEIJU4B\tekur <tekuragari@gmail.com>
2019-06-02 22:36:37 +0900

    入力した文字列のQRコードを生成するサンプルを作成

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>QRコード生成</h1>
+    <qr-code-generator/>
+  </div>
+</template>
+
+<script>
+import QrCodeGenerator from "./components/QRCodeGenerator.vue";
+
+export default {
+  name: "app",
+  components: {
+    QrCodeGenerator
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/QRCodeGenerator.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div>
+      <label>文字列</label>
+      <input type="text" v-model="inputText">
+      <input type="button" @click="generate" value="生成">
+    </div>
+    <vue-qrcode v-if="targetText" :value="targetText" :options="option" tag="img"></vue-qrcode>
+  </div>
+</template>
+
+<script>
+import VueQrcode from "@chenfengyuan/vue-qrcode";
+export default {
+  components: {
+    VueQrcode
+  },
+  data() {
+    return {
+      inputText: "",
+      targetText: "",
+      option: {
+        errorCorrectionLevel: "M",
+        maskPattern: 0,
+        margin: 10,
+        scale: 2,
+        width: 300,
+        color: {
+          dark: "#000000FF",
+          light: "#FFFFFFFF"
+        }
+      }
+    };
+  },
+  methods: {
+    generate: function() {
+      this.targetText = this.inputText;
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
